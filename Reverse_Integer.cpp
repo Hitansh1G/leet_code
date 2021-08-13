0ef5972fbc2fb2df2cb3192cd1365e15db2765c6
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-08-13 17:32:17 +0530

    Reverse_Input

---
+++ Reverse_Integer.cpp
@@ -0,0 +1,20 @@
+//all usual stuff
+class Solution {
+public:
+    int reverse(int x) {
+        int temp=0, rev =0, pop;
+        while(x!=0){
+            pop = x % 10;
+            x = x / 10;
+            temp = rev * 10 + pop;
+            rev = temp;}
+        
+            return rev;
+        
+        return 0;
+            
+            
+        }
+
+    
+};
