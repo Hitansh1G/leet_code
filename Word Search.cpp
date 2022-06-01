8c45a66ac1ad11c5db0e575adf494866915c35e6
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-06-01 21:44:26 +0530

    Word Search

--- Word Search.cpp
+++ Word Search.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-       bool dfs(vector<vector<char>> &arr, string word, int k, int r, int c){                              if(r>=arr.size()||r<0||c>=arr[0].size()||c<0||arr[r][c] != word[k])
-                return false;
-            k++;
-            if(k==word.size()){
-                return true;
-            }
-            arr[r][c]='*'   ;
-                                                                             bool res =                     dfs(arr,word,k,r+1,c) || 
-		    dfs(arr,word,k,r-1,c) || 
-			dfs(arr,word,k,r,c+1) ||
-			dfs(arr,word,k,r,c-1);
-            
-            arr[r][c]=word[k-1];
-            return res;
-        }
-        bool exist(vector<vector<char>>& arr, string word) {
-        for(int i=0 ; i<arr.size() ; i++){
-            for(int j=0 ; j<arr[0].size() ; j++){
-                if(dfs(arr,word,1,i,j)){
-                    return true;
-                }
-            }
-        }
-        return false;
+       bool dfs(vector<vector<char>>& board, string word, vector<vector<int>> &vis, int i , int j, int m , int n, int idx)
+    {
+        if(idx == word.length()) return 1;
+        if(i<0 || i>=m || j<0 || j>=n) return 0;
+        if(board[i][j]!=word[idx]) return 0;
+        if(vis[i][j]) return 0;
+        vis[i][j]=1;
+        bool val = dfs(board, word, vis, i+1, j, m, n, idx+1) ||
+            dfs(board, word, vis, i-1, j, m, n, idx+1) ||
+            dfs(board, word, vis, i, j+1, m, n, idx+1) ||
+            dfs(board, word, vis, i, j-1, m, n, idx+1);
+        vis[i][j]=0;
+        return val;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+       int m = board.size();
+       int n = board[0].size();
+       for(int i=0;i<m;i++)
+       {
+           for(int j=0;j<n;j++)
+           {
+              if(board[i][j] == word[0])
+              {
+                  vector<vector<int>> vis(m, vector<int> (n, 0));
+                  if(dfs(board, word, vis, i, j, m, n, 0)) return 1;
+              }
+           }
+       }
+        return 0;
     }
 };
