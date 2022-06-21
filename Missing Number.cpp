61465413abcb820b303a1a92c2a0d07fa0395ccc
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-06-21 14:35:00 +0530

    Missing Number

---
+++ Missing Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int i=0;
+        sort(nums.begin() , nums.end());
+        for(i=0 ; i<nums.size() ; i++){
+            if(nums[i]==i) continue;
+            else{
+                break;
+            }
+        }
+        return i;
+    }
+};
