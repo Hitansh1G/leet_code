24f938d720b8ebb03fe49b53fcf0f6db167dab70
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-07-18 16:32:32 +0530

    Jump Game III

---
+++ Jump Game III.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        int n=arr.size();
+        if(start>=n || start<0){
+            return false;
+        }
+        if(arr[start]==0){
+            return true;
+        }
+        if(arr[start]==-1){
+            return false;
+        }
+        int temp=arr[start];
+        arr[start]=-1;
+        return canReach(arr,start+temp) || canReach(arr,start-temp);
+    }
+};
