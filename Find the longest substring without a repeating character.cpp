d920494690de9b851a42b8f950773c3ddedd0213
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-07-21 13:59:52 +0530

    Find the longest substring without a repeating character

---
+++ Find the longest substring without a repeating character.cpp
@@ -0,0 +1,66 @@
+#include "../crio/cpp/io/FastIO.hpp"
+#include <bits/stdc++.h>
+using namespace std;
+
+class LongestSubstringWithoutRepeatingCharacter {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        unordered_set<char> st;
+        int i = 0;
+        int j = 0;
+        int ct = 0;
+        int result = 0;
+        
+        while(j<n){
+            if(st.count(s[j])==0){
+                st.insert(s[j]);
+                ct = st.size();
+                result = max(result , ct);
+                j++;
+            }
+            else {
+                st.erase(s[i]);
+                i++;
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    FastIO();
+    string s;
+    getline(cin, s);
+    int result =
+        LongestSubstringWithoutRepeatingCharacter().lengthOfLongestSubstring(s);
+    cout << result;
+    return 0;
+}
+
+/* 
+Crio Methodology
+
+Milestone 1: Understand the problem clearly
+1. Ask questions & clarify the problem statement clearly.
+2. Take an example or two to confirm your understanding of the input/output & extend it to test cases
+Milestone 2: Finalize approach & execution plan
+1. Understand what type of problem you are solving.
+     a. Obvious logic, tests ability to convert logic to code
+     b. Figuring out logic
+     c. Knowledge of specific domain or concepts
+     d. Knowledge of specific algorithm
+     e. Mapping real world into abstract concepts/data structures
+2. Brainstorm multiple ways to solve the problem and pick one
+3. Get to a point where you can explain your approach to a 10 year old
+4. Take a stab at the high-level logic & write it down.
+5. Try to offload processing to functions & keeping your main code small.
+Milestone 3: Code by expanding your pseudocode
+1. Make sure you name the variables, functions clearly.
+2. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+3. Use libraries as much as possible
+Milestone 4: Prove to the interviewer that your code works with unit tests
+1. Make sure you check boundary conditions
+2. Time & storage complexity
+3. Suggest optimizations if applicable
+*/
