8c277ca490471fdfde547c7c4354bf8819422850
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-07-22 13:09:36 +0530

    Find the next greater number with the same set of digits

---
+++ Find the next greater number with the same set of digits.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int nextGreaterElementWithSameSetOfDigits(int num){
+    string str = to_string(num);
+    int n=str.size();
+    int i=n-2;
+    while(i >= 0 && str[i] >= str[i + 1]){
+        i--;
+    }
+    if(i < 0){
+        return -1;
+    }
+    int j = n - 1;
+    while(j >= 0){
+        if(str[j] > str[i]){
+            break;
+        }   
+        j--;
+    }
+    swap(str[i], str[j]);
+    reverse(str.begin() + i + 1, str.end());
+    long long res = stol(str);
+    if(res > INT_MAX || res < INT_MIN)
+        return -1;
+    return res;
+
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<nextGreaterElementWithSameSetOfDigits(n);
+    return 0;
+}
+
+/* 
+Crio Methodology
+
+Milestone 1: Understand the problem clearly
+1. Ask questions & clarify the problem statement clearly.
+2. Take an example or two to confirm your understanding of the input/output & extend it to test cases
+Milestone 2: Finalize approach & execution plan
+1. Understand what type of problem you are solving.
+     a. Obvious logic, tests ability to convert logic to code
+     b. Figuring out logic
+     c. Knowledge of specific domain or concepts
+     d. Knowledge of specific algorithm
+     e. Mapping real world into abstract concepts/data structures
+2. Brainstorm multiple ways to solve the problem and pick one
+3. Get to a point where you can explain your approach to a 10 year old
+4. Take a stab at the high-level logic & write it down.
+5. Try to offload processing to functions & keeping your main code small.
+Milestone 3: Code by expanding your pseudocode
+1. Make sure you name the variables, functions clearly.
+2. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+3. Use libraries as much as possible
+Milestone 4: Prove to the interviewer that your code works with unit tests
+1. Make sure you check boundary conditions
+2. Time & storage complexity
+3. Suggest optimizations if applicable
+*/
