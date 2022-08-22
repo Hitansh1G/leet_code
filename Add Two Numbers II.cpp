52f26c9c1a34caac8349def13efa489c4aaebdd9
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-08-22 20:38:50 +0530

    Add Two Numbers II

---
+++ Add Two Numbers II.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head){
+        if(head == NULL) return head;
+        ListNode* ptr, *fptr, *prev;
+        ptr = head;
+        fptr = ptr->next;
+        ptr-> next = NULL;
+        while(fptr != NULL){
+            prev = ptr;
+            ptr= fptr;
+            fptr = fptr->next;
+            ptr->next = prev;
+        }
+        return ptr;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode();
+        ListNode* ptr = head;
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        int carry = 0;
+        while(l1 && l2){ 
+            ptr->next = new ListNode();
+            ptr = ptr->next;
+            ptr->val = l1->val + l2->val  + carry;
+            carry = ptr->val/10;
+            ptr->val = ptr->val % 10;
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        while(l1){ 
+            ptr->next = new ListNode();
+            ptr = ptr->next;
+            ptr->val = l1->val + carry;
+            carry = ptr->val/10;
+            ptr->val = ptr->val % 10;
+            l1 = l1->next;
+        }
+        while(l2){ 
+            ptr->next = new ListNode();
+            ptr = ptr->next;
+            ptr->val = l2->val  + carry;
+            carry = ptr->val/10;
+            ptr->val = ptr->val % 10;
+            l2 = l2->next;
+        }
+        if(carry){
+            ptr->next = new ListNode();
+            ptr = ptr->next;
+            ptr->val = carry;
+        }
+        head = reverseList(head->next);
+        return head;
+    }
+};
