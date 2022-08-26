023fc93c79f25fcbdfbf5a80a92f0dbbe5c8d798
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-08-26 14:51:11 +0530

    Gray Code

---
+++ Gray Code.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> out;
+        for(int i=0 ; i<(1<<n) ; i++){
+            out.push_back(i ^(i>>1));
+        }
+        return out;
+
+    }
+};
