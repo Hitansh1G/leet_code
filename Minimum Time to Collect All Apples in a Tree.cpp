13e049c54046bc2b1adaf8153354edff443b8f4a
Hitansh1G <hitanshgupta007@gmail.com>
2023-01-11 23:53:39 +0530

    Minimum Time to Collect All Apples in a Tree

---
+++ Minimum Time to Collect All Apples in a Tree.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        vector<int> leastNeighbour(n, INT32_MAX);
+        for (auto & i : edges) {
+            leastNeighbour[i[0]] = min(i[1], leastNeighbour[i[0]]);
+            leastNeighbour[i[1]] = min(i[0], leastNeighbour[i[1]]);
+
+        }
+        vector<bool>mustVisit(n ,false);
+        int j = 0;
+        for (int i = n - 1; i > 0; --i) {
+            if(hasApple[i]){
+                j = i;
+                while(not mustVisit[j] && j != 0){
+                    mustVisit[j] = true;
+                    j = leastNeighbour[j];
+                }
+            }
+        }
+
+        int cntr = 0;
+        for (auto i : mustVisit) {
+            cntr += int(i);
+        }
+
+        return  cntr*2;
+    }
+};
