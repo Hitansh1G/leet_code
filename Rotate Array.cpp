4127612b16063750bb656cf486d01392840ea07c
Hitansh <hitansh.gupta@kristalball.com>
2023-12-12 18:37:19 +0530

    Rotate Array

--- Rotate Array.cpp
+++ Rotate Array.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int>s;
-        while(k--){
-            int temp = nums.back();
-            nums.pop_back();
-            nums.insert(nums.begin() + 0, temp); 
-        }
+        // vector<int>s;
+        // while(k--){
+        //     int temp = nums.back();
+        //     nums.pop_back();
+        //     nums.insert(nums.begin() + 0, temp); 
+        // }
+        k=k%nums.size();
+        reverse(nums.begin(),nums.begin()+(nums.size()-k));
+        reverse(nums.begin()+(nums.size()-k),nums.end());
+        reverse(nums.begin(),nums.end());
+            
     }
 };
